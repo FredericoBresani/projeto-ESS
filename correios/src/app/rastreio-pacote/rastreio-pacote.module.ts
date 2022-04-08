31fda0e7753b8228e7cdebad5ca70508b509184e
weslley-batista <wbh@cin.ufpe.br>
2022-04-08 13:46:30 -0300

    fix app.module

--- correios/src/app/rastreio-pacote/rastreio-pacote.module.ts
+++ correios/src/app/rastreio-pacote/rastreio-pacote.module.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
 import { RastreamentoPacoteComponent } from "./rastreamento-pacote/rastreamento-pacote.component";
 
 @NgModule({
