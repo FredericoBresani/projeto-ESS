f645801dfdcddce9e448020be593230ecec4ebc2
weslley-batista <wbh@cin.ufpe.br>
2022-04-11 16:14:46 -0300

    not

---
+++ correios/src/app/notificacao/notificacao-pacote.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { NotificacoesComponent } from "./notificacoes/notificacoes.component";
+
+@NgModule({
+    declarations: [
+        NotificacoesComponent
+    ],
+    imports: [
+      CommonModule,
+      ReactiveFormsModule,
+    ],
+    providers: [],
+    
+  })
+
+export class notificacoesModule{ }
