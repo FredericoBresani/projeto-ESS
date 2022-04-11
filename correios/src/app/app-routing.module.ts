59e1b8d90db79546df0239a0007a4ab222f5e89b
José Wilson Cavalcante Ferreira Junior <jwcfj@cin.ufpe.br>
2022-04-10 22:50:13 -0300

    inseri o path das paginas de listar pedidos e de listar progresso da rota

--- correios/src/app/app-routing.module.ts
+++ correios/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro-pedidos/cadastro/cadastro.component';
 import { PedidosComponent } from './cadastro-pedidos/pedidos/pedidos.component';
+import { EnviosComponent } from './envio-de-pacote/envios/envios.component';
+import { RotaComponent } from './envio-de-pacote/rota/rota.component';
 
 const routes: Routes = [
   {
@@ -15,7 +17,16 @@ const routes: Routes = [
   {
     path: 'correios/cadastrar-pedido',
     component: CadastroComponent,
-  }
+  },
+  {
+    path: 'correios/envio-de-pacote',
+    component: EnviosComponent,
+  },
+  {
+    path: 'correios/rota',
+    component: RotaComponent,
+  },
+
 ];
 
 @NgModule({
