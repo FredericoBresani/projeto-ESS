669698c1d7043e107022ead78d51981f9ef269c9
unknown <fbs4@cin.ufpe.br>
2022-05-10 20:14:00 -0300

    test de pedidos atualizadas

--- correios/src/app/rastreio-pacote/rastreio-pacote.service.ts
+++ correios/src/app/rastreio-pacote/rastreio-pacote.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
-import { Pedido } from "../shared/models/pedido.model";
+import { Pedido } from "../../../../Common/pedido";
 import { PedidoClient } from "../shared/client/pedido.client";
 
 @Injectable({
@@ -10,7 +10,7 @@ import { PedidoClient } from "../shared/client/pedido.client";
 export class RastreioPacoteService {
 
   constructor(private readonly pedidoClient: PedidoClient) {}
-  
+
   public getPedidos(): Observable<Pedido[]> {
     return this.pedidoClient.getPedidos();
   }
