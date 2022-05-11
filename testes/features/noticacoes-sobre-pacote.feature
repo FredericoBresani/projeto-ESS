3337954e7ee1b423c5b1420dc60f830b8b17004d
PedrovlBarbosa <55454762+PedrovlBarbosa@users.noreply.github.com>
2022-05-10 21:52:48 -0300

    updateAtualizarCaminho
    ts para verificar se o pop-up de dialog está aparecendo

--- testes/features/noticacoes-sobre-pacote.feature
+++ testes/features/noticacoes-sobre-pacote.feature
@@ -3,9 +3,11 @@ Feature: Notificações sobre o pacote
 		Verificar informações no dialog
 			
 
-Scenario: Notificar atualização de pacote Distrito Federal para Goias
-	Given o pedido é devidamente cadastrado com nome "fred - pedido" e a aplicação é redirecionada para a página de pedidos
+Scenario: Notificar atualização de pacote Distrito Federal para Pernambuco
+	Given na pagina de login e efetuo meu login com nome: "pedro" e senha: "123"
+	Given entro na página cadastro de pedido
+	Given os não estão todos preenchidos
+	When preencho todos os campos necessários, considerando nome do pedido como "Macbook para Monitor" e aperto em confirmar pedido
+	When o pedido é cadastrado com nome "pedro - Macbook para monitor" e a aplicação é redirecionada para a página de pedidos
 	When o pedido é atualizado na página de rotas ao pressionar o botão Atualizar Caminho
-	Then o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "pedido" agora está em Goias
-	Then o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "pedido" agora está em Bahia
-	Then o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "pedido" agora está em Pernambuco
+	Then o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "Macbook para Monitor" agora está em Pernambuco
--- testes/typeScript/passos/notificacao.js
+++ testes/typeScript/passos/notificacao.js
@@ -13,30 +13,48 @@ const protractor_1 = require("protractor");
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 cucumber_1.defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^o pedido é cadastrado com nome "fred - pedido" e a aplicação é redirecionada para a página de pedidos$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get("http://localhost:4200/#/correios/pedidos");
+    Given(/^na pagina de login e efetuo meu login com nome: "([^\"]*)" e senha: "([^\"]*)"$/, (nome, senha) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.browser.get("http://localhost:4200/");
+        yield expect(protractor_1.browser.getTitle()).to.eventually.equal("Login");
+        yield protractor_1.$("input[name='login']").sendKeys(nome);
+        yield protractor_1.$("input[name='senha']").sendKeys(senha);
+        yield protractor_1.$("button[name='submit']").click();
+    }));
+    Given(/^entro na página cadastro de pedido$/, () => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.browser.get("http://localhost:4200/#/correios/cadastrar-pedido");
         yield expect(protractor_1.browser.getTitle()).to.eventually.equal("Correios");
     }));
+    Given(/^os não estão todos preenchidos$/, () => __awaiter(this, void 0, void 0, function* () {
+        yield expect(protractor_1.$("input[name='numero_cartao']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='nome']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='cod_seguranca']").getAttribute('value')).to.eventually.equal('0');
+        yield expect(protractor_1.$("input[name='cpf']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='cep']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='nome_pedido']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='peso']").getAttribute('value')).to.eventually.equal('0');
+        yield expect(protractor_1.$("select[name='opcoes']").getAttribute('value')).to.eventually.equal("");
+        yield expect(protractor_1.$("input[name='endereco']").getAttribute('value')).to.eventually.equal("");
+    }));
+    When(/^preencho todos os campos necessários, considerando nome do pedido como "([^\"]*)" e aperto em confirmar pedido$/, (nome_pedido) => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.$("input[name='numero_cartao']").sendKeys('0568');
+        yield protractor_1.$("input[name='nome']").sendKeys('pedro');
+        yield protractor_1.$("input[name='cod_seguranca']").sendKeys(133);
+        yield protractor_1.$("input[name='cpf']").sendKeys('512410');
+        yield protractor_1.$("input[name='cep']").sendKeys('65412');
+        yield protractor_1.$("input[name='nome_pedido']").sendKeys(nome_pedido);
+        yield protractor_1.$("input[name='peso']").sendKeys(10);
+        yield protractor_1.$("input[name='endereco']").sendKeys('PE');
+        yield protractor_1.$("select[name='opcoes']").sendKeys('Entrega Rapida');
+        yield protractor_1.element(protractor_1.by.name('submit')).click();
+    }));
+    When(/^o pedido é cadastrado com nome "pedro - Macbook para monitor" e a aplicação é redirecionada para a página de pedidos$/, () => __awaiter(this, void 0, void 0, function* () {
+        yield protractor_1.browser.get("http://localhost:4200/#/correios/notificacoes");
+    }));
     When(/^o pedido é atualizado na página de rotas ao pressionar o botão Atualizar Caminho$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get("http://localhost:4200/#/correios/rota");
-        yield protractor_1.element(protractor_1.by.name('botaoAtualizar')).click();
-    }));
-    Then(/^o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "([^\"]*)" agora está em Goias$/, (nome_pedido) => __awaiter(this, void 0, void 0, function* () {
-        yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Atualização sobre o seu pacote " + nome_pedido);
-        yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Seu pacote está na unidade de tratamento em Goias");
-        //await expect(browser.getRoles()).to.equal("dialog");
-    }));
-    Then(/^o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "([^\"]*)" agora está em Bahia$/, (nome_pedido) => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get("http://localhost:4200/#/correios/rota");
-        yield protractor_1.element(protractor_1.by.name('botaoAtualizar')).click();
-        yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Atualização sobre o seu pacote " + nome_pedido);
-        yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Seu pacote está na unidade de tratamento em Bahia");
-        //await expect(browser.getRoles()).to.equal("dialog");
+        yield protractor_1.browser.get("http://localhost:4200/#/correios/notificacoes");
+        yield protractor_1.element(protractor_1.by.name('atualizacao')).click();
     }));
     Then(/^o pedido é atualizado na página de rotas e aparece um dialog informando que o pacote "([^\"]*)" agora está em Pernambuco$/, (nome_pedido) => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get("http://localhost:4200/#/correios/rota");
         yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Atualização sobre o seu pacote " + nome_pedido);
-        yield expect(protractor_1.element(protractor_1.by.name("dialog")).getText()).to.eventually.equal("Seu pacote está na unidade de tratamento em Pernambuco");
-        //await expect(browser.getRoles()).to.equal("dialog");
     }));
 });
